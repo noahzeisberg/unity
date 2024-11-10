fe59b452b6f6358c20ca7e2610e4a398c0610650
NoahOnFyre <noahonfyre@gmail.com>
2024-11-10 15:32:45 +0100

    refactor: bump

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt", "@nuxtjs/supabase"],
+  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt"],
   app: {
       head: {
         title: "Eternity — School Organisation System",
